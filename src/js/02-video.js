0c73b2a3a6e0070f0d0c24675d4616ed1f36b186
Bohdan Shevaniuk <113462257+eazzyk@users.noreply.github.com>
2023-04-22 20:11:17 +0300

    і

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,15 +2,13 @@ import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Vimeo(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
 
 player.on(
   'timeupdate',
   throttle(function (data) {
     localStorage.setItem(STORAGE_KEY, data.seconds);
-
-    // console.log('played the video!');
   }, 1000)
 );
 const currentTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
